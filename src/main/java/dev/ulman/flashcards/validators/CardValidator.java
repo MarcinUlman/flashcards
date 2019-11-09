f62d95f004973dd5b88bea260d6b370ee4f4a92f
Marcin Ulman <marcin@ulman.dev>
2019-11-09 22:12:15 +0100

    card validator

--- src/main/java/dev/ulman/flashcards/validators/CardValidator.java
+++ src/main/java/dev/ulman/flashcards/validators/CardValidator.java
@@ -1,6 +1,7 @@
 package dev.ulman.flashcards.validators;
 
 import dev.ulman.flashcards.model.Card;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -8,6 +9,10 @@ import org.springframework.validation.Validator;
 
 @Component
 public class CardValidator implements Validator {
+
+    final int MAE_EXTENSION_LENGTH = 5;
+    final String[] POSSIBLE_EXTENSIONS = {".apng", ".bmp", ".gif", ".ico", ".cur", ".jpg", ".jpeg", ".jfif", ".pjpeg", ".pjp",
+    ".png", ".svg", ".tif", ".tiff", ".webp"};
 
     @Override
     public boolean supports(Class<?> aClass) {
@@ -20,5 +25,33 @@ public class CardValidator implements Validator {
         ValidationUtils.rejectIfEmpty(errors, "word", "Word can't be empty");
         ValidationUtils.rejectIfEmpty(errors, "translation", "Translation can't be empty");
 
+        Card card = (Card) o;
+        String url = card.getImageURL();
+        if(url != null && url.length() > 0){
+            UrlValidator urlValidator = new UrlValidator();
+            if (!urlValidator.isValid(url)){
+                errors.rejectValue("imageURL", "This is not a url");
+            } else {
+                if (isImage(getExtension(url))){
+                    errors.rejectValue("imageURL", "This is forbidden file format");
+                }
+            }
+        }
+    }
+
+    private String getExtension(String url){
+        String extension = url.substring(url.indexOf('.'));
+        if (MAE_EXTENSION_LENGTH > extension.length()){
+            return extension;
+        }
+        return "";
+    }
+
+    private boolean isImage(String extension) {
+        if(extension.length() < 3) return false;
+        for (String ext :  POSSIBLE_EXTENSIONS){
+            if(extension.equals(ext)) return true;
+        }
+        return false;
     }
 }
